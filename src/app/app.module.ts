b4f6b0809de931a484a07bb5d3309b7a5e48d95c
ram <er.rajatmalik@gmail.com>
2017-12-16 15:51:15 +0530

    Update HTML as per template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { SamplePostComponent } from './sample-post/sample-post.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
+// routing start 
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'sample', component: SamplePostComponent},
+  {path: 'contact', component: ContactUsComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
+// routing ends 
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent,
+    AboutUsComponent,
+    SamplePostComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,    
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sample-post/sample-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-post',
+  templateUrl: './sample-post.component.html',
+  styles: []
+})
+export class SamplePostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
